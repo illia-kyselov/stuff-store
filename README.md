4ddfebe92359fc975aa117d37d463f2406de9479
_PaLa4_ <illya.kis99@gmail.com>
2023-09-03 19:42:42 +0300

    result of prj

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-Figma 
+# Used Technologies:
+
+React, React-routes, Redux Toolkit, JS, HTML, CSS
+
+# API:
+
+https://fakeapi.platzi.com/en/rest/introduction
+
+# Figma:
+
+https://www.figma.com/file/7itpNsHVP5LPUpK4ENDYkv/Shop?node-id=0%3A1&t=i8nKSaWzeNTqTOnX-0
+
+
+
+# Demo:
+
+
--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -10,7 +10,7 @@ const Poster = () => (
     <div className={styles.product}>
       <div className={styles.text}>
         <div className={styles.subtitle}>the bestseller of 2022</div>
-        <h1 className={styles.head}>LENNON r2d2 with NVIDIA 5090 TI</h1>
+        <h1 className={styles.head}>LENOVO with NVIDIA 5090 TI</h1>
         <button className={styles.button}>Shop Now</button>
       </div>
       <div className={styles.image}>
